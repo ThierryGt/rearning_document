d0afa12ade4e5e65395526c759ddf5907155f8c2
Gt <yaotingzhan@gmail.com>
2018-12-04 11:11:26 +0800

    flask过滤器

--- flaskapp.py
+++ flaskapp.py
@@ -51,7 +51,128 @@ def get_context_processor():
     return dict(d=data)
 
 
-
 if __name__ == "__main__":
     app.run()
     # mongodb_conn() 
+
+
+# flask 过滤器
+
+# 字符串操作
+{# 当变量未定义时,显示默认字符串,可以缩写为d #}
+<p>{{name | default("No name", true)}}</p>
+
+{# 单词首字母大写 #}
+<p>{{ "hello" | capitalize }}</p>
+
+{# 单词全小写 #}
+<p>{{"XML" | lower}}</p>
+
+{ # 去除字符串前后的空白字符 #}
+<p>{{ " hello " | trim}}</p>
+
+{ # 字符串反转, 返回"olleh" #}
+<p>{{"hello" | reverse}}</p>
+
+{ # 格式化输出,返回“Number is 2” #}
+<p>{{ "%s is %d" | format("Number", 2)}}</p>
+
+{ # 关闭HTML自动转义 #}
+<p>{{"<em>name</em>" | safe}}</p>
+
+{% autoescape false%}
+{ # HTML转义, 即使autoescape关了也转义,可以缩写为e#}
+<p>{{"<em>name</em>"}}</p>
+{% endautoescape%}
+
+
+# 数值操作
+{ # 四舍五入取整, 返回13.0 #}
+<p>{{ 12.8888 | round}}</p>
+
+{ #向下截取到小数点后2位 #}
+<{{ 12.8888 | round(2, "floor")}}>
+
+{ #绝对值,返回12 #}
+<p>{{-12 | abs}}</p>
+
+
+# 列表操作
+{# 取第一个元素}
+<p>{{ [1,2,3,4,5] | first}}</p>
+
+{# 取最后一个元素}
+<p>{{ [1,2,3,4,5] | last}}</p>
+
+{# 返回列表长度, 可以写为count#}
+<p>{{ [1,2,3,4,5] | length}}</p>
+
+{# 列表求和 }
+<p>{{ [1,2,3,4,5] | sum}}</p>
+
+{# 列表排序,默认为升序#}
+<p>{{ [3,2,1,5,4] | sort}}</p>
+
+{# 合并为字符串,返回"1|2|3|4|5"#}
+<p>{{ [1,2,3,4,5] | join("|")}}</p>
+
+{# 列表中所有元素都大写,这里可以用upper, lower， 但capitalize无效#}
+<p>{{ ["tom", "bob", "ada" | upper]}}</p>
+
+
+# 字典列表操作
+{% set users=[{'name':'Tom','gender':'M','age':20},
+              {'name':'John','gender':'M','age':18},
+              {'name':'Mary','gender':'F','age':24},
+              {'name':'Bob','gender':'M','age':31},
+              {'name':'Lisa','gender':'F','age':19}]
+%}
+
+{# 按指定字段排序,这里设reverse为true使其按降序排#}
+<ul>
+{% for user in users | sort(attribute='age', reverse=true) %}
+     <li>{{ user.name }}, {{ user.age }}</li>
+{% endfor %}
+</ul>
+
+{# 列表分组，每组是一个子列表，组名就是分组项的值 #}
+<ul>
+{% for group in users|groupby('gender') %}
+    <li>{{ group.grouper }}<ul>
+    {% for user in group.list %}
+        <li>{{ user.name }}</li>
+    {% endfor %}</ul></li>
+{% endfor %}
+</ul>
+
+
+{# 取字典中的某一项组成列表，再将其连接起来 #}
+<p>{{ users | map(attribute='name') | join(', ') }}</p>
+
+
+# Flask内置过滤器"tojson",变量输出为JSON字符串,可结合Javascript使用
+# safe避免HTML自动转义
+<script type="text/javascript">
+var = users = {{users | tojson | safe}};
+console.log(user[0].name);
+</script>
+
+
+# 自定义过滤器
+def double_step_filter(l):
+    return l[::2]
+app.add_template_filter(double_step_filter, "double_step")
+函数的第一个参数是过滤器函数,第二个参数是过滤器名称,
+{ # 返回[1,3,5]}
+<p>{{ [1,2,3,4,5] | double_step}}</p>
+
+# 过滤器装饰器"template_filter"
+@app.template_filter("sub")
+def sub(l, start, end):
+    return l[start:end]
+# 返回[2,3,4]
+<p>{{ [1,2,3,4,5] | sub(1,4)}}</p>
+
+
+
+
